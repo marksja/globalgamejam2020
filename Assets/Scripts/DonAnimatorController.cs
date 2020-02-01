90e713c88861eef74b2a56374ee0fa6b96f91b7f
Bevans104 <bryceevans104@gmail.com>
2020-02-01 15:49:16 -0500

    Set up a basic Front Door Sequence scene, imported Don and set up animations and materials, created a simple DonAnimatorController script for playing animations within Mecanim.

---
+++ Assets/Scripts/DonAnimatorController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class DonAnimatorController : MonoBehaviour
+{
+    Animator anim;
+
+	void Start()
+	{
+		anim = GetComponent<Animator>();
+	}
+
+	public void PlayAnimation(string animationToPlay)
+	{
+		anim.Play(animationToPlay);
+	}
+
+	[ContextMenu("Play Idle")]
+	void PlayITest()
+	{
+		anim.Play("Idle");
+	}
+	[ContextMenu("Play Greeting")]
+	void PlayGTest()
+	{
+		anim.Play("Greeting");
+	}
+	[ContextMenu("Play Negative")]
+	void PlayNTest()
+	{
+		anim.Play("Negative");
+	}
+}
