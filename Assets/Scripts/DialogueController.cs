32ad5a39130aba26ca00b6ff32a999a6bdc13599
Bevans104 <bryceevans104@gmail.com>
2020-02-01 19:33:57 -0500

    Added a diaolgue box prefab for both sides of the conversation, some UI elements and fonts, basic scene setup, and script for passing in dialogue to show.

---
+++ Assets/Scripts/DialogueController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueController : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+
+	public void SetText(string inText)
+	{
+		text.text = inText;
+	}
+
+	[ContextMenu("Test text")]
+	public void TestText()
+	{
+		SetText("Hey this is some dialogue! Is this working? If it isn't, you wouldn't even see that question.");
+	}
+}
